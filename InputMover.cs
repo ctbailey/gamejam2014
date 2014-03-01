5086009e62ae5cc4402290ae1ddd2f707bf4f434
ctbailey <christopher.thomas.bailey@gmail.com>
2014-03-01 15:41:40 -0500

    Fixed some jumping issues and Time.deltaTime.

--- InputMover.cs
+++ InputMover.cs
@@ -5,9 +5,10 @@ public class InputMover : MonoBehaviour {
 	
 	public CharacterController controller;
 	public float gravity = 0.1f;
-	public float jumpSpeed = 1.0f;
+	public float startingJumpSpeed = 1.0f;
 	public float horizontalSpeed = 50.0f;
-	private float jumpAcceleration = 0f;
+	private float currentJumpSpeed = 0f;
+	private int count = 0;
 	
 	// Use this for initialization
 	void Start () {
@@ -16,13 +17,13 @@ public class InputMover : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 horizontalTranslation = Update_MoveHorizontal();
-        Vector3 verticalTranslation = Update_MoveVertical();
 		if(Input.GetButtonDown("Jump")
 			&& controller.isGrounded)
 		{
 			Jump();
 		}
+		Vector3 verticalTranslation = Update_MoveVertical();
+		Vector3 horizontalTranslation = Update_MoveHorizontal();
 		controller.Move(horizontalTranslation + verticalTranslation);
 	}
 	Vector3 Update_MoveHorizontal()
@@ -33,14 +34,18 @@ public class InputMover : MonoBehaviour {
 	}
 	Vector3 Update_MoveVertical()
 	{
-		jumpAcceleration -= gravity;
-		Vector3 verticalMove = new Vector3(0, jumpAcceleration, 0);
-		Debug.Log(verticalMove);
+		if(!controller.isGrounded)
+		{
+			currentJumpSpeed -= gravity;	
+		}
+		
+		Vector3 verticalMove = new Vector3(0, currentJumpSpeed, 0) * Time.deltaTime;
 		return verticalMove;
 	}
 	void Jump()
 	{	
-		Debug.Log("Jumping");
-		jumpAcceleration = jumpSpeed;
+		Debug.Log("Jump " + count);
+		count++;
+		currentJumpSpeed = startingJumpSpeed;
 	}
 }
