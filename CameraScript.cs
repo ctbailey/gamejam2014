7c6fb932fe6fe06657130f3350b284ad7535c90f
zaneev10 <zaneev10@yahoo.com>
2014-03-02 15:03:50 -0500

    t

--- CameraScript.cs
+++ CameraScript.cs
@@ -41,8 +41,8 @@ public class CameraScript : MonoBehaviour {
 		if(show)
 		{
 			GUI.Box(new Rect(20,20,Screen.width - 40,Screen.height - 40), background);
-			GUI.Box(new Rect(20,20,Screen.width - 40,Screen.height - 40), "Do it to it");
-			if(GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 100, 100, 50), "Begin"))
+			GUI.Box(new Rect(20,20,Screen.width - 40,Screen.height - 40), "Zombies in the Disco! Navigate to the end to hit the button and end this nightmare. \nDirections:\nA = Left\nD = Right\nSpace Bar = Jump\nDon't get dead as disco. ");
+			if(GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height - 100, 100, 50), "Do it to it!"))
 			{
 				show = false;
 				Time.timeScale = 1;
